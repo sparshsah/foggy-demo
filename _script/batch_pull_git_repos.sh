dbac9a74293e1d2bd8d84271c15547f06355490c
Sparsh Sah <sparshsah@gmail.com>
2021-01-02 12:27:44 -0500

    master -> main

--- _script/batch_pull_git_repos.sh
+++ _script/batch_pull_git_repos.sh
@@ -3,6 +3,7 @@
 LOCAL_REPO_PATH="/code/$(id -u -n)/"  # e.g. "/code/sparshsah/"
 REPO_NAMES=$(ls $LOCAL_REPO_PATH | grep /)
 
+
 prompt_until_yes() {
   echo "$1, ok? ('y' to continue/skip, 'Ctrl+C' to quit)"
   CONFIRM_INPUT="n"
@@ -14,6 +15,12 @@ prompt_until_yes() {
 exec_except_prompt() {
   $@ || prompt_until_yes "..error executing '$*' (exit status code $?, recommend quitting to debug)"
 }
+
+git_checkout_main() {
+  # market leader GitHub has switched its default branch name, but not all devs have accepted
+  git checkout main || git checkout master
+}
+
 
 prompt_until_yes "pulling from remote(s) to $LOCAL_REPO_PATH"
 prompt_until_yes "pulling repos ${REPO_NAMES[*]}"
@@ -36,11 +43,11 @@ for REPO_NAME in ${REPO_NAMES[@]}; do
   GIT_STATUS=$(git status)
   echo "$GIT_STATUS" | sed "/^$/d"
   # hacky.. git's "nothing to see here" status message is 4 lines long
-  [[ $(echo "$GIT_STATUS" | wc -l) -gt 4 ]] && prompt_until_yes "..git status check failed"
+  [[ $(echo "$GIT_STATUS" | wc -l) -ne 4 ]] && prompt_until_yes "..git status check failed"
 
-  echo "..switching to master and pulling.."
-  # TODO(sparshsah): why can't i just `(checkout && pull)` or `{ checkout && pull; }`?
-  exec_except_prompt git checkout master
+  echo "..switching to main and pulling.."
+  # TODO(sparshsah): why can't i just use `(checkout && pull)` or `{ checkout && pull; }`?
+  exec_except_prompt git_checkout_main
   exec_except_prompt git pull
 
   echo
