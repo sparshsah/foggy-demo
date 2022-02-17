b81a74f5e6150a1e34198b3e6c87ad464cd7105f
Sparsh Sah <sparshsah@gmail.com>
2022-02-17 01:08:38 -0800

    exe

--- demo/cs/_sandbox/a.cpp
+++ demo/cs/_sandbox/a.cpp
@@ -1,6 +1,6 @@
 /*
 you can compile this (using beta C++20 standard support and printing all warnings) by e.g.
-$ clang++ {sourcefile_name}.cpp -std=c++2a -Wall -o {outfile_name}.out
+$ clang++ {sourcefile_name}.cpp -std=c++2a -Wall -o {sourcefile_name}.exe
 */
 
 #include <vector>
