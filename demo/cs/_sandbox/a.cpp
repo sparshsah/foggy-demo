2047273b5cabc9c878a87854880e1737e7ee90c7
Sparsh Sah <sparshsah@gmail.com>
2022-02-17 00:59:11 -0800

    greet others

--- demo/cs/_sandbox/a.cpp
+++ demo/cs/_sandbox/a.cpp
@@ -1,16 +1,19 @@
 /*
-you can compile this (printing warnings) by e.g.
+you can compile this (using beta C++20 standard support and printing all warnings) by e.g.
 $ clang++ {sourcefile_name}.cpp -std=c++2a -Wall -o {outfile_name}.out
 */
 
+#include <vector>
 #include <iostream>
 
 
 int main(int argc, char* argv[]) {
+    // don't include the invoked name
+    std::vector<std::string> args(argv + 1, argv + argc);
     std::string greeting = "Hello world";
-    /* for (char* arg : argv) {
-        greeting += "and " + str(arg);
-    } */
+    for (std::string arg : args) {
+        greeting += " and " + arg;
+    }
     greeting += "!\n";
     std::cout << greeting;
     return 0;
