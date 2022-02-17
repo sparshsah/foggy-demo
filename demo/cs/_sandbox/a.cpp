78d129f65b9239bf64fe6a0c5cba3233b3f1aeb5
Sparsh Sah <sparshsah@gmail.com>
2022-02-17 00:48:25 -0800

    cpp

---
+++ demo/cs/_sandbox/a.cpp
@@ -0,0 +1,17 @@
+/*
+you can compile this (printing warnings) by e.g.
+$ clang++ {sourcefile_name}.cpp -Wall -o {outfile_name}.out
+*/
+
+#include <iostream>
+
+
+int main(int argc, char* argv[]) {
+    std::string greeting = "Hello world";
+    /* for (char* arg : argv) {
+        greeting += "and " + str(arg);
+    } */
+    greeting += "!\n";
+    std::cout << greeting;
+    return 0;
+}
