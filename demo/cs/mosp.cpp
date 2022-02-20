555cf0a46d487e5fd074a3a6f96bc0c645f4c713
Sparsh Sah <sparshsah@gmail.com>
2022-02-19 18:21:28 -0800

    explain_vec_init

--- demo/cs/mosp.cpp
+++ demo/cs/mosp.cpp
@@ -16,7 +16,12 @@ You can compile this (using C++20 standard support and printing all warnings) by
 
 
 int main(int argc, char* argv[]) {
-    // don't include the invoked name
+    // Initialize a vector of strings, using the constructor that takes in an array,
+    // and covers `n` elements of it.
+    // If we didn't already know the length of the array, we'd have to
+    // divide the size of the array by the size of the data type it holds:
+    // n = sizeof(arr) / sizeof(arr[0])`.
+    // Don't include the invoked name (argv[0]).
     std::vector<std::string> args(argv + 1, argv + argc);
     std::string greeting = "Hello world";
     for (std::string arg : args) {
